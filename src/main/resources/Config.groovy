b56a2574eb5bfcb55f0ca3bea29316e2decaf461
Marcelo Adamatti <adamatti@gmail.com>
2015-12-11 20:37:43 -0200

    MONGOLAB_URI

--- src/main/resources/Config.groovy
+++ src/main/resources/Config.groovy
@@ -1,9 +1,12 @@
 env = System.env.ENV ?: "dev"
 
 mongo = [
+	user   : "",
+	passs  : "",
 	host   : "docker.me",
 	port   : 27017,
-	dbname : System.env.MONGO_DB_NAME ?: "adamatti-wiki-${env}" 
+	dbname : System.env.MONGO_DBNAME ?: "adamatti-wiki-${env}",
+	url    : System.env.MONGOLAB_URI ?: "mongodb://${mongo.user}:${mongo.pass}@${mongo.host}:${mongo.port}/${mongo.dbname}"
 ]
 
 spark = [
---
+++ src/test/groovy/adamatti/MongoTest.groovy
@@ -0,0 +1,22 @@
+package adamatti
+
+import com.mongodb.DB
+import com.mongodb.MongoClient
+import com.mongodb.MongoClientURI
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * Created by Marcelo on 11/12/2015.
+ */
+class MongoTest {
+	@Test
+	public void testConnect(){
+		String uri = System.env.MONGOLAB_URI
+		MongoClientURI mongoClientURI = new MongoClientURI(uri)
+		MongoClient mongoClient = new MongoClient(mongoClientURI)
+		DB db = mongoClient.getDB(mongoClientURI.getDatabase())
+		db.authenticate(mongoClientURI.getUsername(), mongoClientURI.getPassword())
+		Assert.assertNotNull(db)
+	}
+}
