3f72ec478d96f05dd1abcfd95fe8962613c1a3c5
Marcelo Adamatti <adamatti@gmail.com>
2016-01-21 15:11:37 -0200

    Moved processors to DI

--- src/main/groovy/adamatti/bizo/TiddlerRenderBO.groovy
+++ src/main/groovy/adamatti/bizo/TiddlerRenderBO.groovy
@@ -1,41 +1,51 @@
 package adamatti.bizo
 
+import groovy.util.logging.Slf4j
 import org.asciidoctor.Asciidoctor
 import org.markdown4j.Markdown4jProcessor
-import org.asciidoctor.Asciidoctor.Factory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
-import adamatti.commons.TemplateHelper;
+import adamatti.commons.TemplateHelper
 import adamatti.model.dao.TiddlerDAO
 import adamatti.model.entity.Tiddler
+
+@Slf4j
 @Service
 class TiddlerRenderBO {
 	@Autowired
+	private Asciidoctor asciidoctor
+
+	@Autowired
+	private Markdown4jProcessor markdown
+
+	@Autowired
 	private TiddlerDAO tiddlerDao
 
+	//TODO refactor this
 	public String process(Tiddler tiddler){
 		if (tiddler == null){
 			return ""
 		} else if (tiddler.type == "markdown") {
-			return new Markdown4jProcessor().process(tiddler.body)
+			return markdown.process(tiddler.body)
 		} else if (tiddler.type == "asciidoctor") {
 			def options  = [:]
-			return Factory.create().convert(tiddler.body,options)
+			return asciidoctor.convert(tiddler.body,options)
 		} else if (tiddler.type == "groovy+md"){
 			Map variables = [tiddlers:tiddlerDao.findAll()]
 			String result = TemplateHelper.processTemplate(tiddler.body,variables)
 
-			result = new Markdown4jProcessor().process(result)
+			result = markdown.process(result)
 			return result
 		} else if (tiddler.type == "groovy+ad"){
 			Map variables = [tiddlers:tiddlerDao.findAll()]
 			String result = TemplateHelper.processTemplate(tiddler.body,variables)
 
 			def options = [:]
-			return Factory.create().convert(result,options)
+			return asciidoctor.convert(result,options)
 		}
 
-		return "Type not found: ${tiddler.type}"
+		log.trace("Type not found [tiddlerName: ${tiddler.name}, type: ${tiddler.type}] ")
+		return tiddler.body
 	}
 }
