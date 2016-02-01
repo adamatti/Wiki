ad22d12ba9594b1f81394721222f0c941c718b41
Marcelo Adamatti <adamatti@gmail.com>
2016-02-01 16:58:09 -0200

    Added timer to start

--- src/main/groovy/adamatti/WikiMain.groovy
+++ src/main/groovy/adamatti/WikiMain.groovy
@@ -1,26 +1,33 @@
 package adamatti
 
 import org.springframework.context.support.ClassPathXmlApplicationContext
-
+import org.springframework.util.StopWatch
 import spark.Spark
 import adamatti.commons.Resources
-import groovy.util.logging.Slf4j;
+import groovy.util.logging.Slf4j
 
 @Slf4j
 class WikiMain {
-	private static int port = "${Resources.cfg.spark.port}".toInteger() 
-	
+	private static int port = "${Resources.cfg.spark.port}".toInteger()
+
 	public static void main(String [] args){
+		log.info("Server starting...")
+
+		StopWatch clock = new StopWatch(); clock.start()
+
 		startSpark()
-		startSpring()		
-		log.info "Server started on ${port}"
+		startSpring()
+
+		clock.stop()
+
+		log.info "Server started on ${port} - ${clock.getTotalTimeMillis()} ms"
 	}
-	
+
 	private static startSpark(){
 		Spark.port(port)
 		//Spark.externalStaticFileLocation(new File("build/bower").path)
 	}
-	
+
 	private static startSpring(){
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/META-INF/spring-app.xml")
 		context.registerShutdownHook()
